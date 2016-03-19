96953c1bba0830bfdb3d3150be189bc608e91a2f
brianxautumn <shred@brianautumn.com>
2016-03-18 20:18:47 -0700

    Connection Works!

--- VideoClient/src/Network/NetworkManager.js
+++ VideoClient/src/Network/NetworkManager.js
@@ -6,6 +6,8 @@ Flare.NetworkManager= function (videoPlayer) {
     * @property Flare.VideoPlayer} videoPlayer - A reference to the videoPlayer.
     */
     this.videoPlayer = videoPlayer;
+    
+    this.socket = new WebSocket('ws://localhost:6661' );
 
     return this;
     
