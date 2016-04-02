a15fb858aee740593e33f7f9d727a8d842ec1e29
brianxautumn <shred@brianautumn.com>
2016-04-01 19:15:53 -0700

    Started adding code for parsing json

--- VideoClient/build/FlareMediaPlayer.0.0.0.js
+++ VideoClient/build/FlareMediaPlayer.0.0.0.js
@@ -237,6 +237,8 @@ Flare.MediaPlayer.prototype = {
 
     },
     
+    
+    //This should probably go in videoEngine
     update: function (time) {
         this.clock.update(time);
         
@@ -246,9 +248,9 @@ Flare.MediaPlayer.prototype = {
 
         //Finished with update, render the frame:
         //TESTING ONLY
-        
         this.canvas.render(this.frames[this.testCounter%152]);
         this.testCounter++;
+        //Timing is completely messed up. Need to figure out core video engine code
     }
     
   
@@ -285,15 +287,15 @@ Flare.NetworkManager = function (mediaPlayer) {
      */
     this.mediaPlayer = mediaPlayer;
     this.socket;
-    
+
     this.connected = false;
-    
+
     this.callbacks = {};
-    
+
     this.connect();
     this.setup();
-    
-    
+
+
     return this;
 
 
@@ -301,17 +303,15 @@ Flare.NetworkManager = function (mediaPlayer) {
 };
 
 Flare.NetworkManager.prototype = {
-    
-    connect: function(){
-        
+    connect: function () {
+
         this.socket = new WebSocket('ws://localhost:6661');
+        this.socket.binaryType = 'arraybuffer';
     },
-    
-    close: function(){
-        
+    close: function () {
+
         this.socket.close();
     },
-    
     setup: function () {
 
         this.socket.onopen = this.onOpen.bind(this);
@@ -319,34 +319,62 @@ Flare.NetworkManager.prototype = {
         this.socket.onmessage = this.onMessage.bind(this);
         this.socket.onerror = this.onError.bind(this);
 
-        
+
     },
-    
-    onOpen: function(){
-        
+    onOpen: function () {
+
         console.log("connection Opened");
         this.socket.send("hello");
 
     },
-    
-    onClose: function(){
+    onClose: function () {
         console.log("closed");
     },
-    
-    onMessage: function(message){
-        console.log(message.data);
+    onMessage: function (message) {
+        
+        if (message.data instanceof ArrayBuffer ){
+            
+            //All incoming binary should be images
+            console.log(message);
+            
+        }else if (typeof message.data === "string"){
+            
+            //Process as json
+
+            var data = JSON.parse(message.data);
+            console.log(data);
+            
+            
+        }
+        //var data = message.data;
+        
     },
-    
-    onError: function(e){
+    onError: function (e) {
         console.log(e);
     },
-    
-    send:function(data){
-        
+    send: function (data) {
+
         this.socket.send(data);
-        
+
+    },
+    getEndianness: function () {
+        var a = new ArrayBuffer(4);
+        var b = new Uint8Array(a);
+        var c = new Uint32Array(a);
+        b[0] = 0xa1;
+        b[1] = 0xb2;
+        b[2] = 0xc3;
+        b[3] = 0xd4;
+        if (c[0] === 0xd4c3b2a1) {
+            return BlobReader.ENDIANNESS.LITTLE_ENDIAN;
+        }
+        if (c[0] === 0xa1b2c3d4) {
+            return BlobReader.ENDIANNESS.BIG_ENDIAN;
+        } else {
+            throw new Error('Unrecognized endianness');
+        }
     }
-    
+
 };
 
 Flare.NetworkManager.prototype.constructor = Flare.NetworkManager;
